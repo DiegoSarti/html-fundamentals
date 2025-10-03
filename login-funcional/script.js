2f115ed652353b735d521c8183830f41fd5d7041
DIEGO SARTI ARAUJO VIANA <diego.saviana@senacsp.edu.br>
2025-10-02 22:27:48 -0300

    login funcional aula 02/10

---
+++ login-funcional/script.js
@@ -0,0 +1,23 @@
+function loginSenac()
+{
+   let usuario = document.getElementById('txtUsuario').value;
+   let senha = document.getElementById('txtSenha').value;
+   let menasagem = document.getElementById('mensagem');
+
+   if(usuario == 'admin' && senha == '12345')
+   {
+    menasagem.style.color = 'green';
+    menasagem.innerText = 'Usuario Correto';
+
+    setTimeout(() => {
+        window.location.href = 'https://www.google.com.br';
+    }, 5000);
+    
+
+   }
+   else
+    {
+    menasagem.style.color = 'red';
+    menasagem.innerText = 'Usuario/senha invalida';
+   }
+}
